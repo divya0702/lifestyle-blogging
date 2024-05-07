f653de8fcade74a31267b13415b1cfdee2dfb11a
divya0702 <divyakamila7@gmail.com>
2024-05-07 19:11:46 +0530

    adding router

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,24 @@ import HomePage from './HomePage'; // Import the HomePage component
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer';
 
-
 const App: React.FC = () => {
   return (
     <>
-    <div style={{backgroundColor: '#EEE2DC'}}>
-    <div style={{fontFamily:'Dancing Script, cursive', marginBlock:'0 4rem', color:'#123c69'}}>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Use HomePage the main home page */}
-        <Route path="/technology" element={<PostList category="technology" />} />
-        <Route path="/travel" element={<PostList category="travel" />} />
-        <Route path="/food" element={<PostList category="food" />} />
-        <Route path="/post/:id" element={<IndividualPost />} />
-      </Routes>
-      <Footer/>
-    </Router>
-    </div>
-    </div>
+      <div style={{ backgroundColor: '#EEE2DC' }}>
+        <div style={{ fontFamily: 'Dancing Script, cursive', marginBlock: '0 4rem', color: '#123c69' }}>
+          <Router basename="/lifestyle-blogging"> {/* Specify the basename */}
+            <Header />
+            <Routes>
+              <Route path="/" element={<HomePage />} /> {/* Use HomePage the main home page */}
+              <Route path="/technology" element={<PostList category="technology" />} />
+              <Route path="/travel" element={<PostList category="travel" />} />
+              <Route path="/food" element={<PostList category="food" />} />
+              <Route path="/post/:id" element={<IndividualPost />} />
+            </Routes>
+            <Footer />
+          </Router>
+        </div>
+      </div>
     </>
   );
 };
